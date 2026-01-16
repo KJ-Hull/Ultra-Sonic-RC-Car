fcf5f0a3fbd0cdd55df8812c29825aecb69ae821
21601593 <keegan.j.hull@gmail.com>
2026-01-16 20:56:09 +0200

    Various changes - GUI project creation, main firmware .ino file created, and various helper .ino files for RC car functionality

---
+++ BT Serial Interface/serial_comms.cpp
@@ -0,0 +1,8 @@
+//
+// Created by keega on 2026/01/16.
+//
+
+#include "serial_comms.h"
+
+namespace sercomm {
+} // sercomm
---
+++ BT Serial Interface/serial_comms.h
@@ -0,0 +1,9 @@
+#ifndef BT_SERIAL_INTERFACE_SERIAL_COMMS_H
+#define BT_SERIAL_INTERFACE_SERIAL_COMMS_H
+
+namespace sercomm {
+    class serial_comms {
+    };
+} // sercomm
+
+#endif //BT_SERIAL_INTERFACE_SERIAL_COMMS_H
